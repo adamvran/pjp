a92f6b8e3204b2509029d5a8983f8d9ad0ac63d6
Adam Vrána <adamvran@seznam.cz>
2023-03-06 19:42:57 +0100

    cv1 ukol hotovy

---
+++ cv1/main.py
@@ -0,0 +1,12 @@
+import re
+
+for i in range(int(input())):
+    expr = input()
+    if re.match(r'^[\d+\-*/()\s]+$', expr) and '**' not in expr and '//' not in expr and '%' not in expr:
+        try:
+            result = eval(expr)
+            print(result)
+        except Exception as e:
+            print("ERROR", e)
+    else:
+        print("ERROR")
